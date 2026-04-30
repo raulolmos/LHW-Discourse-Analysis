cbcfde238b6544e526654a080b180fd12ecee2fe
raulolmos <61280254+raulolmos@users.noreply.github.com>
2026-04-30 15:36:00 +0200

    Delete colab directory

--- colab/step 2.py
+++
@@ -1,8 +0,0 @@
-import shutil
-from google.colab import files
-
-# Comprimimos la carpeta de resultados
-shutil.make_archive('Corpus_LHW_Procesado', 'zip', '/content/LHW-Discourse-Analysis/data/processed/corpus_files')
-
-# Descargamos el archivo zip resultante
-files.download('Corpus_LHW_Procesado.zip')
